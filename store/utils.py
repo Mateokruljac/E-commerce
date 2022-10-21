67cb1bcba12c99bfa412c4d9779cc331554695a7
Mateo Kruljac <mateokruljac23@gmail.com>
2022-10-21 09:33:45 +0200

    Add guestOrder function

--- store/utils.py
+++ store/utils.py
@@ -40,4 +40,45 @@ def cookieCart(request):
     return context
 
 def guestOrder (request,data):
+    print("User is not logged in !")
+    print("COOKIES:",request.COOKIES["cart"])
+    username = data["form"]["name"]
+    email = data["form"]["email"]
+    cookieData= cookieCart(request)
+    items = cookieData["items"]
+    
+    #it is neccessery to automatically create a new customer, 
+    # check if user with that username or email already exits
+    if Customer.objects.filter(email = email).exists() or  Customer.objects.filter(name = username).exists():
+        pass
+    else:
+        customer = Customer.objects.create(
+            email = email,
+            name = username,
+        )
+        customer.save()
+    
+    #create a order
+    order = Order.objects.create(
+        customer = customer,
+        complete = False
+    ),
+    
+    for item in items:
+        product = Product.objects.get(id = item["product"]["id"])        
+        orderItem = OrderItem.objects.get_or_create(
+            product = product,
+            order = order,
+            quantity = item["quantity"]
+        )
+    
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data["shipping"]["address"],
+            city = data["shipping"]["city"],
+            state = data["shipping"]["state"],
+            zip_code = data["shipping"]["zipcode"]
+        ).save()
     return " "
